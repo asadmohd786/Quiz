430f741db0beb7886627dbd2e56c4adf2767a3d5
asadmohd786 <92883306+asadmohd786@users.noreply.github.com>
2022-12-18 12:35:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+const quizData = [{
+    question: "The correct sequence of HTML tags for starting a webpage is - ",
+    a: "Head, Title, HTML, body",
+    b: "HTML, Body, Title, Head",
+    c: "HTML, Head, Title, Body",
+    d: "HTML, Head, Title, Body",
+    correct: "d",
+},
+{
+    question: "Which of the following element is responsible for making the text bold in HTML?",
+
+    a: "<pre>",
+    b: "<a>",
+    c: "<b>",
+    d: "<br>",
+    correct: "c",
+},
+{
+    question:  " <input> is -",
+
+    a: "a format tag.",
+    b: "an empty tag.",
+    c: "All of the above",
+    d: "None of the above",
+
+    correct: "a",
+},
+{
+    question: "What does CSS stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+}
+];
+let index = 0;
+let correct = 0,
+incorrect = 0,
+total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type='radio']")
+const loadQuestion = () => {
+if (total === index) {
+    return quizEnd()
+}
+reset()
+const data = quizData[index]
+questionBox.innerHTML = `${index + 1}) ${data.question}`
+allInputs[0].nextElementSibling.innerText = data.a
+allInputs[1].nextElementSibling.innerText = data.b
+allInputs[2].nextElementSibling.innerText = data.c
+allInputs[3].nextElementSibling.innerText = data.d
+}
+
+document.querySelector("#submit").addEventListener(
+"click",
+function() {
+    const data = quizData[index]
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+    index++;
+    loadQuestion()
+}
+)
+
+const getAnswer = () => {
+let ans;
+allInputs.forEach(
+    (inputEl) => {
+        if (inputEl.checked) {
+            ans = inputEl.value;
+        }
+    }
+)
+return ans;
+}
+
+const reset = () => {
+allInputs.forEach(
+    (inputEl) => {
+        inputEl.checked = false;
+    }
+)
+}
+
+const quizEnd = () => {
+// console.log(document.getElementsByClassName("container"));
+document.getElementsByClassName("container")[0].innerHTML = `
+    <div class="col">
+        <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
+    </div>
+`
+}
+loadQuestion(index);
